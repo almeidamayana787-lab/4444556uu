1388e9503d9391afaed9c2c0b1f08041d8222b3b
Carlos <cdanieloffice@gmail.com>
2026-03-06 19:16:44 -0400

    fix: corrigindo namespace broken do VipController

--- routes/groups/api/profile/vip.php
+++ routes/groups/api/profile/vip.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\Profile\VipController;
+use App\Http\Controllers\VipController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('vip')
-    ->group(function ()
-    {
+    ->group(function () {
         Route::get('/', [VipController::class, 'index']);
     });
