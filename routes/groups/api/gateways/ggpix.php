dfac09fc7508b8fd8b287395e39d7722eeb4ec75
Carlos <cdanieloffice@gmail.com>
2026-03-06 19:07:07 -0400

    fix: adicionando rota POST para webhook do ggpix

--- routes/groups/api/gateways/ggpix.php
+++ routes/groups/api/gateways/ggpix.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('ggpix')
     ->group(function () {
         Route::post('consult-status-transaction', [DepositController::class, 'consultStatusTransactionPix']);
+        Route::post('webhook', [\App\Http\Controllers\Gateway\GgpixController::class, 'callbackMethod']);
     });
