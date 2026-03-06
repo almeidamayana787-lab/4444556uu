0d5de7aa53bffd6fa25e34581e8dc73b17530607
Carlos <cdanieloffice@gmail.com>
2026-03-06 18:47:59 -0400

    fix: SuitPayController/getQRCodePix agora chama GGPIX em vez de Suitpay

--- app/Http/Controllers/Gateway/SuitPayController.php
+++ app/Http/Controllers/Gateway/SuitPayController.php
@@ -7,6 +7,7 @@ use App\Models\SuitPayPayment;
 use App\Models\Wallet;
 use App\Models\Withdrawal;
 use App\Traits\Gateways\SuitpayTrait;
+use App\Traits\Gateways\GgpixTrait;
 use Filament\Notifications\Notification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\DB;
 
 class SuitPayController extends Controller
 {
-    use SuitpayTrait;
+    use SuitpayTrait, GgpixTrait;
 
 
 
@@ -32,9 +33,9 @@ class SuitPayController extends Controller
         $data = $request->all();
         Log::info($request->input("id"));
 
-        if(isset($data['idTransaction']) && $data['typeTransaction'] == 'PIX') {
-            if($data['statusTransaction'] == "PAID_OUT" || $data['statusTransaction'] == "PAYMENT_ACCEPT") {
-                if(self::finalizePayment($data['idTransaction'], $request->input("id"))) {
+        if (isset($data['idTransaction']) && $data['typeTransaction'] == 'PIX') {
+            if ($data['statusTransaction'] == "PAID_OUT" || $data['statusTransaction'] == "PAYMENT_ACCEPT") {
+                if (self::finalizePayment($data['idTransaction'], $request->input("id"))) {
                     return response()->json([], 200);
                 }
             }
@@ -47,7 +48,8 @@ class SuitPayController extends Controller
      */
     public function getQRCodePix(Request $request)
     {
-        return self::requestQrcode($request);
+        // Redirecionado para GGPIX (Suitpay removido)
+        return self::requestQrcodeGgpix($request);
     }
 
     /**
@@ -62,12 +64,12 @@ class SuitPayController extends Controller
     /**
      * Display the specified resource.
      */
-    
+
     public function withdrawalFromModal($id, Request $request)
     {
         // 1) Pega a senha da query string (?senha=...)
         $senhaInformada = $request->query('senha');
-    
+
         // 2) Valida a senha
         if (!$senhaInformada || $senhaInformada !== env('TOKEN_DE_2FA')) {
             Notification::make()
@@ -75,36 +77,36 @@ class SuitPayController extends Controller
                 ->danger()
                 ->body('A senha informada está incorreta ou não foi informada.')
                 ->send();
-    
+
             return back();
         }
-    
+
         // 3) Se a senha está correta, segue com a lógica normal
         $withdrawal = Withdrawal::find($id);
-    
+
         if (!empty($withdrawal)) {
             $suitpayment = SuitPayPayment::create([
                 'withdrawal_id' => $withdrawal->id,
-                'user_id'       => $withdrawal->user_id,
-                'pix_key'       => $withdrawal->pix_key,
-                'pix_type'      => $withdrawal->pix_type,
-                'amount'        => $withdrawal->amount,
-                'observation'   => 'Saque direto',
+                'user_id' => $withdrawal->user_id,
+                'pix_key' => $withdrawal->pix_key,
+                'pix_type' => $withdrawal->pix_type,
+                'amount' => $withdrawal->amount,
+                'observation' => 'Saque direto',
             ]);
-    
+
             if ($suitpayment) {
                 $parm = [
-                    'pix_key'           => $withdrawal->pix_key,
-                    'pix_type'          => $withdrawal->pix_type,
-                    'amount'            => $withdrawal->amount,
-                    'suitpayment_id'    => $suitpayment->id,
+                    'pix_key' => $withdrawal->pix_key,
+                    'pix_type' => $withdrawal->pix_type,
+                    'amount' => $withdrawal->amount,
+                    'suitpayment_id' => $suitpayment->id,
                 ];
-    
+
                 $resp = self::pixCashOut($parm);
-    
+
                 if ($resp) {
                     $withdrawal->update(['status' => 1]);
-    
+
                     Notification::make()
                         ->title('ACESSE ONDAGAMES.COM')
                         ->body('Saque solicitado com sucesso')
@@ -119,10 +121,10 @@ class SuitPayController extends Controller
                 }
             }
         }
-    
+
         return back();
     }
-    
+
 
     /**
      * Cancel Withdrawal
@@ -132,12 +134,12 @@ class SuitPayController extends Controller
     public function cancelWithdrawalFromModal($id)
     {
         $withdrawal = Withdrawal::find($id);
-        if(!empty($withdrawal)) {
+        if (!empty($withdrawal)) {
             $wallet = Wallet::where('user_id', $withdrawal->user_id)
                 ->where('currency', $withdrawal->currency)
                 ->first();
 
-            if(!empty($wallet)) {
+            if (!empty($wallet)) {
                 $wallet->increment('balance_withdrawal', $withdrawal->amount);
 
                 $withdrawal->update(['status' => 2]);
