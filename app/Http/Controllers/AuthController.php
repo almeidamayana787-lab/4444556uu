d7ae6ce113b24aa636416a891b54091b78ec0567
Carlos <cdanieloffice@gmail.com>
2026-03-14 15:13:32 -0400

    Fix syntax error in AuthController (missing semicolon) correctly via rewrite

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
         return response()->json([
             'message' => 'Login successful',
             'user' => $user
-        ]);
+        ]);;
     }
 
     public function me(Request $request)
